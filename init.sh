9de9e1d83e3ae2f5b1e20201aa12240aca1d794c
root <grandfa8@gmail.com>
2018-03-10 16:00:09 +0300

    add init.sh for prepare env; create ex nginx.conf

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+/etc/init.d/mysql start
